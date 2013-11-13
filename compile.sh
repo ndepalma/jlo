c5772925b70d873b1315ff5f6e5ff8df183f6b93
Thomas Brand <tom@trellis.ch>
2013-11-13 01:02:04 +0100

    1.6 compatibility

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@
 mkdir -p ./classes
 
 echo "compiling `find ./src/ch/lowres/jlo/*.java | wc -l` java files (main)..."
-javac -cp .:./lib/jna-3.3.0.jar src/ch/lowres/jlo/*.java -d classes
+javac -source 1.6 -cp .:./lib/jna-3.3.0.jar src/ch/lowres/jlo/*.java -d classes
 
 echo "compiling `find ./src/ch/lowres/jlo/test/*.java | wc -l` java files (test)..."
-javac -cp .:./classes/:./lib/jna-3.3.0.jar src/ch/lowres/jlo/test/*.java -d classes
+javac -source 1.6 -cp .:./classes/:./lib/jna-3.3.0.jar src/ch/lowres/jlo/test/*.java -d classes
